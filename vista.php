447f688bba7328f4cf87fa7912823ab7d8a9e337
jmanuelcv <manuelcv2587@gmail.com>
2022-03-28 10:11:10 +0200

    Creación de vista

---
+++ vista.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista</title>
+
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <td>#</td>
+                <td>Titulo</td>
+                <td>Autor</td>
+                <td>Fecha</td>
+            </tr>
+        </thead>
+        <tbody>
+    <?php 
+    include "conexion.php";
+foreach ($datos as $key => $value) {
+ echo "<tr>
+<td>value['id']</td> 
+<td>value['titulo']</td> 
+<td>value['autor']</td> 
+<td>value['fecha']</td> 
+ ";
+}
+    ?>
+
+
+
+
+
+        </tbody>
+
+
+    </table>
+</body>
+</html>
+
